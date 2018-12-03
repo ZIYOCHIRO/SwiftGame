96e05cd3fb8ab9d4f1e4a88c7f270a933f8870e2
ZIYOCHIRO <ziyochiro@gmail.com>
2018-12-03 09:59:00 +0800

    properties: 1.make the enemy automatically run itself

--- Space Invaders/Space Invaders/GameScene.swift
+++ Space Invaders/Space Invaders/GameScene.swift
@@ -14,6 +14,29 @@ class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "playerShip")
     let bulletSound = SKAction.playSoundFileNamed("laser.mp3", waitForCompletion: false)
     
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    // Game area: An area you can see on every device
+    var gameArea: CGRect
+    
+    override init(size: CGSize) {
+        
+        let maxAspectRation: CGFloat = 16.0/9.0
+        let playableWidth = size.height / maxAspectRation
+        let margin = (size.width - playableWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // This function will run as soon as the scene loads up
     override func didMove(to view: SKView) {
         // Create background
@@ -29,9 +52,20 @@ class GameScene: SKScene {
         player.zPosition = 2
         self.addChild(player)
         
+        
+        // add enemy aotumatically
+        startNewLevel()
     }
 
-    
+    // make the enemy spawn itself and forever
+    func startNewLevel() {
+        let spawn = SKAction.run(spawnEnemy)
+        let waitToSpawn = SKAction.wait(forDuration: 1)
+        let spawnSequence = SKAction.sequence([spawn, waitToSpawn])
+        let spawnForever = SKAction.repeatForever(spawnSequence)
+        self.run(spawnForever)
+        
+    }
     
     func fireBullet() {
         
@@ -50,6 +84,34 @@ class GameScene: SKScene {
         bullet.run(bulletSequence)
     }
     
+    func spawnEnemy() {
+        // generate the random position
+        let randomXStart = random(min: gameArea.minX, max: gameArea.maxY)
+        let randomXEnd = random(min: gameArea.minY, max: gameArea.maxY)
+        
+        let startPoint = CGPoint(x: randomXStart, y: self.size.height * 1.2)
+        let endPoint = CGPoint(x: randomXEnd, y: self.size.height * 0.2)
+        
+        let enemy = SKSpriteNode(imageNamed: "enemyShip")
+        enemy.setScale(1)
+        enemy.position = startPoint
+        enemy.zPosition = 2
+        self.addChild(enemy)
+        // move down to the screen
+        let moveEnemy = SKAction.move(to: endPoint, duration: 1.5)
+        let deleteEnemy = SKAction.removeFromParent()
+        // run in order
+        let enemySequence = SKAction.sequence([moveEnemy, deleteEnemy])
+        enemy.run(enemySequence)
+        
+        // difference between the startpoint and endPoint
+        let dx = endPoint.x - startPoint.x
+        let dy = endPoint.y - startPoint.y
+        let amountToRotate = atan2(Float(dy), Float(dx))
+        enemy.zRotation = CGFloat(amountToRotate)
+        
+    }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         // run whenever you touch the screen
@@ -59,12 +121,21 @@ class GameScene: SKScene {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch: AnyObject in touches {
+            
             // where we touches the screen
             let pointOfTouch = touch.location(in: self)
             let previousPointOfTouch = touch.previousLocation(in: self)
             // how big the gap is
             let amountDragged = pointOfTouch.x - previousPointOfTouch.x
             player.position.x += amountDragged
+            
+            // make sure the player is in the gameArea
+            if player.position.x  > gameArea.maxX - player.size.width/2 {
+                player.position.x = gameArea.maxX - player.size.width/2
+            }
+            if player.position.y > gameArea.maxY + player.size.width/2{
+                player.position.y = gameArea.maxY + player.size.width/2
+            }
         }
     }
 }
